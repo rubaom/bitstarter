686db669f9015d3913391e2097c9ee9039ebb812
Ruben Adamyan <ruboam@gmail.com>
2013-07-11 04:59:13 +0000

    fixed

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var fs = require("fs");
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-	var str = fs.readFile("index.html").toString();
+	var str = fs.readFileSync("index.html").toString();
   response.send(str);
 });
 
